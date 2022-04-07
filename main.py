d2a9356be980e1a4c113b49a4f7e2e7a0d4b3cf2
Wang Xingyu <1293739194@qq.com>
2022-04-07 15:09:22 +0800

    init

---
+++ main.py
@@ -0,0 +1,9 @@
+'''
+Copyright: Copyright (c) 2021 WangXingyu All Rights Reserved.
+Description: 
+Version: 
+Author: WangXingyu
+Date: 2022-04-06 20:50:54
+LastEditors: WangXingyu
+LastEditTime: 2022-04-06 20:50:55
+'''

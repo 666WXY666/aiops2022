062cac58f76003f7073082716ae600aca8b938e5
Wang Xingyu <1293739194@qq.com>
2022-04-24 22:30:01 +0800

    update ticket bugs

--- utils/submit/submit.py
+++ utils/submit/submit.py
@@ -1,3 +1,12 @@
+'''
+Copyright: Copyright (c) 2021 WangXingyu All Rights Reserved.
+Description: 
+Version: 
+Author: WangXingyu
+Date: 2022-04-20 10:31:37
+LastEditors: WangXingyu
+LastEditTime: 2022-04-24 22:26:03
+'''
 """
 Example for submit.
 
@@ -5,14 +14,14 @@ Before running this script, execute
 
     pip3 install requests
 """
-import json
 
-import requests
 
 # 提交答案服务域名或IP, 将在赛前告知
+import json
+import requests
 HOST = "http://10.3.2.40:30083"
 # 团队标识, 可通过界面下方权限获取, 每个ticket仅在当前赛季有效，如未注明团队标识，结果不计入成绩
-TICKET = "your team ticket"
+TICKET = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxNTAyMTQ3MjI3NjU2MDA3NzA5IiwiaWF0IjoxNjUwODEwMzQ5LCJ0aWNrZXQiOnsidGlkIjoiMTUwMjE0NzIyNzY1NjAwNzcwOSIsImNpZCI6IjE0OTYzOTg1MjY0Mjk3MjQ3NjAiLCJzZWFzb24iOiIxIiwic3RhcnQiOiIxNjUwMzg0MDAwMDAwIiwiZW5kIjoiMTY1MjYzMDM5OTAwMCJ9LCJpc3MiOiJCaXpzZWVyIiwiZXhwIjoxNjUyNjMwMzk5fQ.HH48VoYYlZxvSjsvcGXsjgROkckdJwLko5Dwc_yA1IwCDgOVOn_wZ_1-7Y8ExTQ6fjcokggHnWkrqKVGPFi44Q"
 
 
 def submit(ctx):
